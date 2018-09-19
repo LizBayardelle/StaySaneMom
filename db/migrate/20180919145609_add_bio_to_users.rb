a3063de39e7cd1909921d185b4d4205449ffeffa
Liz Bayardelle <lizbayardelle@gmail.com>
2018-09-19 13:58:47 -0700

    Avatar not foreign key duh

--- db/migrate/20180919145609_add_bio_to_users.rb
+++ db/migrate/20180919145609_add_bio_to_users.rb
@@ -1,6 +1,6 @@
 class AddBioToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :avatar, foreign_key: true
+    add_reference :users, :avatar
     add_column :users, :bio, :text
     add_column :users, :tagline, :string
   end
