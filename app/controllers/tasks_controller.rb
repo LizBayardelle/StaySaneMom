52b2fb2471470894bb579a5206be8649b4700c98
Liz Bayardelle <lizbayardelle@gmail.com>
2018-09-24 13:24:50 -0700

    Other Blogs Bug

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,96 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_variables, only: [:index, :create, :destroy, :check_task, :uncheck_task]
+
+
+  def set_variables
+    @user_tasks = Task.where(user_id: current_user.id)
+    @one_time = Task.where(frequency: "OneTime", completed: false, user_id: current_user.id)
+    @one_time_done = Task.where(frequency: "OneTime", completed: true, user_id: current_user.id)
+    @daily = Task.where(frequency: "Daily", completed: false, user_id: current_user.id)
+    @daily_done = Task.where(frequency: "Daily", completed: true, user_id: current_user.id)
+    @weekly = Task.where(frequency: "Weekly", completed: false, user_id: current_user.id)
+    @weekly_done = Task.where(frequency: "Weekly", completed: true, user_id: current_user.id)
+    @monthly = Task.where(frequency: "Monthly", completed: false, user_id: current_user.id)
+    @monthly_done = Task.where(frequency: "Monthly", completed: true, user_id: current_user.id)
+  end
+
+  # GET /tasks
+  def index
+    @create_task = Task.new
+    @tasks = Task.all
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
+    if @task.save
+      respond_to do |format|
+        format.js
+        format.html
+      end
+    else
+      render :new
+    end
+  end
+
+  # DELETE /tasks/1
+  def destroy
+    if @task.destroy
+        respond_to do |format|
+          format.js
+          format.html
+      end
+    else
+      flash[:warning] = "Oops! Something went wrong!"
+    end
+  end
+
+  def check_task
+      @task = Task.find(params[:id])
+      if @task.update_attributes(completed: true)
+          respond_to do |format|
+            format.js
+            format.html
+          end
+      else
+          redirect_to tasks_path
+          flash[:warning] = "Oops! Something went wrong!"
+      end
+  end
+
+  def uncheck_task
+      @task = Task.find(params[:id])
+      if @task.update_attributes(completed: false)
+          respond_to do |format|
+            format.js
+            format.html
+          end
+      else
+          redirect_to tasks_path
+          flash[:warning] = "Oops! Something went wrong!"
+      end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def task_params
+      params.require(:task).permit(:name, :frequency, :completed, :user_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_205522) do
+ActiveRecord::Schema.define(version: 2018_09_24_031911) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -91,6 +91,16 @@ ActiveRecord::Schema.define(version: 2018_09_23_205522) do
     t.string "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "frequency"
+    t.boolean "completed"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
