476c11eb7aaaf8af5dcac2cda773d1beb4cedda5
Liz Bayardelle <lizbayardelle@gmail.com>
2018-09-26 13:47:41 -0700

    To Do Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_194733) do
+ActiveRecord::Schema.define(version: 2018_09_26_202207) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_194733) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "frequency"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
