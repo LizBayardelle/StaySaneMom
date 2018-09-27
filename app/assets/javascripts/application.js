26739d3226ce891ed9e8991d19e509bf92c42845
Liz Bayardelle <lizbayardelle@gmail.com>
2018-09-27 14:04:39 -0700

    Google Analytics Tracking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,17 @@ $(document).on("ready page:load", function() {
 });
 
 
+
+// GOOGLE ANALYTICS
+
+$(document).on('page:change', function() {
+ if (window._gaq != null) {
+  return _gaq.push(['_trackPageview']);
+ } else if (window.pageTracker != null) {
+  return pageTracker._trackPageview();
+ }
+});
+
 // Match Height JS below
 
 
