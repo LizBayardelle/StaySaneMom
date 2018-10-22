57c84e7df01d01c64d49fc25a68324db6a9a7423
Liz Bayardelle <lizbayardelle@gmail.com>
2018-10-21 20:36:24 -0700

    Froala Replaces Trix

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'high_voltage', '~> 3.1'
 gem 'convertkit-ruby', require: 'convertkit'
 gem 'dotenv-rails'
 gem 'acts_as_list'
+gem "wysiwyg-rails"
+gem "font-awesome-sass"
 
 group :production do
   gem 'pg', '~> 0.20.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,27 @@
 //= require jquery-ui/widget
 //= require jquery-ui/widgets/sortable
 //= require rails-ujs
+//= require froala_editor.min.js
+//= require plugins/align.min.js
+//= require plugins/char_counter.min.js
+//= require plugins/code_beautifier.min.js
+//= require plugins/code_view.min.js
+//= require plugins/colors.min.js
+//= require plugins/font_size.min.js
+//= require plugins/fullscreen.min.js
+//= require plugins/image.min.js
+//= require plugins/image_manager.min.js
+//= require plugins/inline_style.min.js
+//= require plugins/line_breaker.min.js
+//= require plugins/link.min.js
+//= require plugins/lists.min.js
+//= require plugins/paragraph_format.min.js
+//= require plugins/paragraph_style.min.js
+//= require plugins/quote.min.js
+//= require plugins/special_characters.min.js
+//= require plugins/url.min.js
+
+$('selector').froalaEditor();
 
 // ALERT FADE AWAY
 $(document).on("ready page:load", function() {
