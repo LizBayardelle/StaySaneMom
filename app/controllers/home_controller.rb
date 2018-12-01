8c3755ff2fe999c50929de788f0c7b82884fd412
Liz Bayardelle <lizbayardelle@gmail.com>
2018-12-01 14:39:45 -0800

    Hotfix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,53 +8,53 @@ class HomeController < ApplicationController
   end
 
   def house
-    @all_house_blogs = Blog.where(category: "House", published: true).order("published_on DESC")
-    @recent_blogs = Blog.where(category: "House", published: true).order("published_on DESC").limit(3)
+    @all_house_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC")
+    @recent_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
     if params[:tag]
-      @recent_blogs = Blog.where(category: "House", published: true).order("published_on DESC").tagged_with(params[:tag])
-      @other_blogs = Blog.where(category: "House", published: true).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
+      @recent_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag])
+      @other_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
     else
-      @recent_blogs = Blog.where(category: "House", published: true).order("published_on DESC").limit(3)
-      @other_blogs = Blog.where(category: "House", published: true).order("published_on DESC").offset(3).all
+      @recent_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
+      @other_blogs = Blog.where(category: "House").where("published_on <= ?", Date.today).order("published_on DESC").offset(3).all
     end
     @tags = Blog.tag_counts_on(:tags)
   end
 
   def spouse
-    @all_spouse_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC")
-    @recent_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC").limit(3)
+    @all_spouse_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC")
+    @recent_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
     if params[:tag]
-      @recent_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC").tagged_with(params[:tag])
-      @other_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
+      @recent_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag])
+      @other_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
     else
-      @recent_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC").limit(3)
-      @other_blogs = Blog.where(category: "Spouse", published: true).order("published_on DESC").offset(3).all
+      @recent_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
+      @other_blogs = Blog.where(category: "Spouse").where("published_on <= ?", Date.today).order("published_on DESC").offset(3).all
     end
     @tags = Blog.tag_counts_on(:tags)
   end
 
   def kids
-    @all_kids_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC")
-    @recent_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC").limit(3)
+    @all_kids_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC")
+    @recent_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
     if params[:tag]
-      @recent_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC").tagged_with(params[:tag])
-      @other_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
+      @recent_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag])
+      @other_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
     else
-      @recent_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC").limit(3)
-      @other_blogs = Blog.where(category: "Kids", published: true).order("published_on DESC").offset(3).all
+      @recent_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
+      @other_blogs = Blog.where(category: "Kids").where("published_on <= ?", Date.today).order("published_on DESC").offset(3).all
     end
     @tags = Blog.tag_counts_on(:tags)
   end
 
   def self
-    @all_self_blogs = Blog.where(category: "Self", published: true).order("published_on DESC")
-    @recent_blogs = Blog.where(category: "Self", published: true).order("published_on DESC").limit(3)
+    @all_self_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC")
+    @recent_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
     if params[:tag]
-      @recent_blogs = Blog.where(category: "Self", published: true).order("published_on DESC").tagged_with(params[:tag])
-      @other_blogs = Blog.where(category: "Self", published: true).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
+      @recent_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag])
+      @other_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC").tagged_with(params[:tag]).offset(3).all
     else
-      @recent_blogs = Blog.where(category: "Self", published: true).order("published_on DESC").limit(3)
-      @other_blogs = Blog.where(category: "Self", published: true).order("published_on DESC").offset(3).all
+      @recent_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC").limit(3)
+      @other_blogs = Blog.where(category: "Self").where("published_on <= ?", Date.today).order("published_on DESC").offset(3).all
     end
     @tags = Blog.tag_counts_on(:tags)
   end
