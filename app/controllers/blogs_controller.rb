0c99f28c80d09c549c5c9090163104a4b69f518c
Liz Bayardelle <lizbayardelle@gmail.com>
2019-01-01 16:17:46 -0800

    Controller Blog Logic

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,14 +9,14 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.where("published_on <= ?", Date.today).order('published_on DESC').paginate(:page => params[:page], :per_page => 12)
-    @commented_blogs = Blog.where("published_on <= ?", Date.today).order('comments_count DESC').limit(3)
+    @blogs = Blog.where(published: true).where("published_on <= ?", Date.today).order('published_on DESC').paginate(:page => params[:page], :per_page => 12)
+    @commented_blogs = Blog.where(published: true).where("published_on <= ?", Date.today).order('comments_count DESC').limit(3)
     if params[:tag]
-      @blogs = Blog.where("published_on <= ?", Date.today).order('published_on DESC').tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 12)
+      @blogs = Blog.where(published: true).where("published_on <= ?", Date.today).order('published_on DESC').tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 12)
     else
-      @blogs = Blog.where("published_on <= ?", Date.today).order('published_on DESC').paginate(:page => params[:page], :per_page => 12)
+      @blogs = Blog.where(published: true).where("published_on <= ?", Date.today).order('published_on DESC').paginate(:page => params[:page], :per_page => 12)
     end
-    @unpublished = Blog.where.not("published_on <= ?", Date.today)
+    @unpublished = Blog.where(published: false)
     @tags = Blog.tag_counts_on(:tags)
   end
 
