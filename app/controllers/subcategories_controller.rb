49482fb99bb4070cb8cc5fad7f2641aed7c473b0
Liz Bayardelle <lizbayardelle@gmail.com>
2019-06-01 10:28:55 -0700

    About Us and Subcategories

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -4,7 +4,7 @@ class SubcategoriesController < ApplicationController
   # GET /subcategories
   # GET /subcategories.json
   def index
-    @subcategories = Subcategory.order('name DESC')
+    @subcategories = Subcategory.order('category ASC')
   end
 
   # GET /subcategories/1
