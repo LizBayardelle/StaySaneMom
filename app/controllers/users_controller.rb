5221bf7a80a3f8d332423cb0c3eebcbb77217d27
Liz Bayardelle <lizbayardelle@gmail.com>
2019-08-24 10:53:33 -0700

    Hotfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,21 @@ class UsersController < ApplicationController
       @new_members = []
     end
 
-    @purchases = Purchase.where(user_id: current_user.id)
-    
-    @done_onetime_tasks = Task.where(user_id: current_user.id, completed: true, frequency: "OneTime")
-    @onetime_tasks = Task.where(user_id: current_user.id, frequency: "OneTime")
-    @done_daily_tasks = Task.where(user_id: current_user.id, completed: true, frequency: "Daily")
-    @daily_tasks = Task.where(user_id: current_user.id, frequency: "Daily")
-    @done_weekly_tasks = Task.where(user_id: current_user.id, completed: true, frequency: "Weekly")
-    @weekly_tasks = Task.where(user_id: current_user.id, frequency: "Weekly")
-    @done_monthly_tasks = Task.where(user_id: current_user.id, completed: true, frequency: "Monthly")
-    @monthly_tasks = Task.where(user_id: current_user.id, frequency: "Monthly")
+    @purchases = Purchase.where(user_id: @user.id)
 
-    @people_birthday = Person.where(user_id: current_user.id).where.not(birthday: nil).sort_by{ |b| [b.birthday.month, b.birthday.day]}.first(5)
+    @done_onetime_tasks = Task.where(user_id: @user.id, completed: true, frequency: "OneTime")
+    @onetime_tasks = Task.where(user_id: @user.id, frequency: "OneTime")
+    @done_daily_tasks = Task.where(user_id: @user.id, completed: true, frequency: "Daily")
+    @daily_tasks = Task.where(user_id: @user.id, frequency: "Daily")
+    @done_weekly_tasks = Task.where(user_id: @user.id, completed: true, frequency: "Weekly")
+    @weekly_tasks = Task.where(user_id: @user.id, frequency: "Weekly")
+    @done_monthly_tasks = Task.where(user_id: @user.id, completed: true, frequency: "Monthly")
+    @monthly_tasks = Task.where(user_id: @user.id, frequency: "Monthly")
 
-    @occasions = Occasion.where(user_id: current_user.id).where("date > ?", Time.now).order("date ASC")
-    @gifts = Gift.where(user_id: current_user.id, purchased: false)
+    @people_birthday = Person.where(user_id: @user.id).where.not(birthday: nil).sort_by{ |b| [b.birthday.month, b.birthday.day]}.first(5)
+
+    @occasions = Occasion.where(user_id: @user.id).where("date > ?", Time.now).order("date ASC")
+    @gifts = Gift.where(user_id: @user.id, purchased: false)
 
     @comments = Comment.where.not(blog_id: nil)
     @new_comments = Comment.where("created_at >= ?", Time.zone.now.beginning_of_week).where.not(blog_id: nil)
