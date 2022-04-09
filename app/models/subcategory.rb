7b37b2fbb8b80e8aa493091a7093caca1df26608
Dr. Liz Bayardelle <liz@Drs-MacBook-Pro.local>
2022-04-09 16:04:25 -0700

    Hotfix

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,12 +1,12 @@
 class Subcategory < ApplicationRecord
   # belongs_to :category
 
-  has_many :subcategorization_blogs
-  has_many :blogs, through: :subcategorization_blogs
+  has_many :subcategorization_blogs, dependent: :delete_all
+  has_many :blogs, through: :subcategorization_blogs, dependent: :delete_all
 
-  has_many :subcategorization_freebies
-  has_many :freebies, through: :subcategorization_freebies
+  has_many :subcategorization_freebies, dependent: :delete_all
+  has_many :freebies, through: :subcategorization_freebies, dependent: :delete_all
 
-  has_many :subcategorization_solutions
-  has_many :solutions, through: :subcategorization_solutions
+  has_many :subcategorization_solutions, dependent: :delete_all
+  has_many :solutions, through: :subcategorization_solutions, dependent: :delete_all
 end
